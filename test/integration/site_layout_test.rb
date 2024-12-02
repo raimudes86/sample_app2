3c94c10d2672a056085d1bee27560b1fbd445fa7
raimu <raijirou12@gmail.com>
2024-12-02 09:27:11 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
   #このlayout linksはこのテスト自体の名前だからなんでもいい
   test "layout links" do
     get root_path
@@ -10,9 +13,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get root_path
+    log_in_as(@user)
+    #ログインの処理の時にユーザーのページにリダイレクトが飛んでいるからテストでは自動的に行えないリダイレクトをさせなきゃいけなかった
+    follow_redirect!
+    assert_select "a[href=?]", users_path, count: 1
   end
 end
