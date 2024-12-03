5f59ca554e1fcf62c3423253d0d4183bc189deec
raimu <raijirou12@gmail.com>
2024-12-03 16:43:25 +0900

    localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  # host = 'https://sample-app2-cogv.onrender.com'
-  host = 'localhost:3000'
+  host = 'https://sample-app2-cogv.onrender.com'
+  # host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
